67d4a6bbe5334cd159f8a1d1069e0e8be8335db2
darshLogicgo <darsh.logicgo@gmail.com>
2025-03-18 11:51:27 +0530

    Fixed port binding for Render

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/', // ✅ Ensure base path is correct
+  server: {
+    host: "0.0.0.0", // 👈 Ye line add karo
+    port: process.env.PORT || 5173, // 👈 Render ka assigned port use karo
+  },
 });
